a9e17725337864a8c1f4a35d57ee7670a1d7bde1
Luan Castheloge <luanccp@gmail.com>
2023-08-07 11:04:55 +0100

    test: input not with loading

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -2,11 +2,19 @@ import { render, screen } from "@testing-library/react-native";
 import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
-  it("should be render without activity indicator", () => {
+  it("should be render WITHOUT activity indicator", () => {
     render(<Input />);
-    
+
     const activityIndicator = screen.queryByTestId("activity-indicator");
 
     expect(activityIndicator).toBeNull();
+  });
+
+  it("should be render WITH activity indicator", () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId("activity-indicator");
+
+    expect(activityIndicator).toBeTruthy();
   });
 });
